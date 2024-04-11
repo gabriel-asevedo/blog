d5a5e423ea50accbcbe5e91fd00df06a4e2cd22b
Gabriel_Asevedo <gabrielamorim002@gmail.com>
2024-04-11 15:59:18 -0300

    new validations added

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,8 @@
 class Comment < ApplicationRecord
   include Visible
-  
+
   belongs_to :article
+
+  validates :commenter, presence: true, length: {minimum: 4, maximum:25}
+  validates :body, presence: true
 end
